33d510570767ea0fc2188c007cddb1ac2fc725b4
Ian Ross <ianrossdrums@gmail.com>
2022-09-21 10:24:36 -0600

    test: add subscription cancel tests

--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -32,4 +32,42 @@ RSpec.describe 'The subscription API endpoints' do
     expect(sub[:attributes][:customer_id]).to eq(customer.id)
     expect(sub[:attributes][:tea_id]).to eq(tea.id)
   end
+  it 'cancels a tea subscription' do
+    customer = create(:customer)
+    tea = create(:tea)
+    sub = create(:subscription, customer_id: customer.id, tea_id: tea.id)
+    params = {
+      sub_id: sub.id,
+      status: "Canceled"
+    }
+
+    expect(sub[:status]).to eq("Active")
+
+    patch "/api/v1/subscribe/cancel", params: params
+
+    sub = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(sub).to include :id, :type, :attributes
+    expect(sub[:attributes]).to include :title, :price, :status, :frequency, :customer_id, :tea_id
+
+    expect(sub[:attributes][:status]).to eq("Canceled")
+  end
+
+  describe "sad path" do
+    it "can't update status" do
+      customer = create(:customer)
+      tea = create(:tea)
+      sub = create(:subscription, customer_id: customer.id, tea_id: tea.id)
+      params = {
+        sub_id: sub.id,
+        status: nil
+      }
+
+      patch "/api/v1/subscribe/cancel", params: params
+
+      expect(response).to_not be_successful
+    end
+  end
 end
