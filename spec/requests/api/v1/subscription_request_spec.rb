95c04b294e39b0e3c0f687b433426b84c60f1541
Ian Ross <ianrossdrums@gmail.com>
2022-09-21 09:56:26 -0600

    test: create subscription test

--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -8,11 +8,28 @@ RSpec.describe 'The subscription API endpoints' do
       title: "Silver",
       price: 2.00,
       status: "Active",
-      frequency: "Once a month",
+      frequency: "Monthly",
       customer_id: customer.id,
       tea_id: tea.id
     }
 
     get "/api/v1/subscribe", params: params
+
+    sub = JSON.parse(response.body, symbolize_names: true)[:data]
+    new_sub = Subscription.last
+
+    expect(response).to be_successful
+
+    expect(sub).to include :id, :type, :attributes
+    expect(sub[:id]).to eq(new_sub.id)
+    expect(sub[:type]).to eq("subscription")
+
+    expect(sub[:attributes]).to include :title, :price, :status, :frequency, :customer_id, :tea_id
+    expect(sub[:attributes][:title]).to eq("Silver")
+    expect(sub[:attributes][:price]).to eq(2.00)
+    expect(sub[:attributes][:status]).to eq("Active")
+    expect(sub[:attributes][:frequency]).to eq("Monthly")
+    expect(sub[:attributes][:customer_id]).to eq(customer.id)
+    expect(sub[:attributes][:tea_id]).to eq(tea.id)
   end
 end
