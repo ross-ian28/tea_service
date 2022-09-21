cd5e60895a519de469625eed4a242be51a67a007
Ian Ross <ianrossdrums@gmail.com>
2022-09-21 10:24:46 -0600

    feat: subscription controllers

--- app/controllers/api/v1/subscription_controller.rb
+++ app/controllers/api/v1/subscription_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::SubscriptionController < ApplicationController
   def create
-    new_sub = Subscription.create!(sub_params)
+    new_sub = Subscription.create(sub_params)
     render json: SubscriptionSerializer.subscription(new_sub)
+  end
+
+  def update
+    sub = Subscription.find(params[:sub_id])
+    if sub.update(status: params[:status])
+      render json: SubscriptionSerializer.subscription(sub)
+    else
+      render status: 404
+    end
   end
 
   private
