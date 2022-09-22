9a476f369b72e2046927682fb9771c7d70dbb9de
Ian Ross <ianrossdrums@gmail.com>
2022-09-21 18:08:41 -0600

    feat: all subscriptions route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "/subscribe", to: "subscription#create"
+      get "/subscriptions", to: "subscription#index"
       patch "/subscribe/cancel", to: "subscription#update"
     end
   end
